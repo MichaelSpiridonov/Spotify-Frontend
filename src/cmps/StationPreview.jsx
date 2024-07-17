4d7fe5e73e0b0805fc2e3e195581c1b4d5d7bb79
Amit Gal <amitgal@hotmail.com>
2024-07-17 22:25:26 +0300

    Add basic StationPreview.jsx

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,9 +1,24 @@
+import React from 'react'
 
-export function StationPreview() {
-    return (
-        <section>
-            <h1>Station Preview</h1>
-        </section>
-    )
-    
+export function StationPreview({ station }) {
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp)
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+  }
+
+  return (
+    <ul className='StationPreview'>
+      {station.songs.map((song) => (
+        <li key={song.id}>
+          <button>▶</button>
+          <img src={song.imgUrl} alt={song.title} />
+          <span>{song.title}</span>
+          <span></span> {/* Placeholder for song album */}
+          <span>{formatDate(song.addedAt)}</span>
+          <button>+</button>
+          <span>0:00</span> {/* Placeholder for song length */}
+        </li>
+      ))}
+    </ul>
+  )
 }
