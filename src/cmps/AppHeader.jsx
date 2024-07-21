0257676e69799c98a19379e2abf3f99934dbe85e
Amit Gal <amitgal@hotmail.com>
2024-07-21 16:03:10 +0300

    Improve styling of navigation buttons in StationDetails and AppHeader

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,19 +1,33 @@
+import { useNavigate } from 'react-router-dom'
 
 export function AppHeader() {
-	return (
-		<header className="app-header">
-			
-			<section className="header-container">
-				<section className="pageing">
-					<section className="page-backward">
-						<svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16"><path d="M11.03.47a.75.75 0 0 1 0 1.06L4.56 8l6.47 6.47a.75.75 0 1 1-1.06 1.06L2.44 8 9.97.47a.75.75 0 0 1 1.06 0z"></path></svg>
-					</section >
-					<section className="page-forward">
-						<svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16"><path d="M4.97.47a.75.75 0 0 0 0 1.06L11.44 8l-6.47 6.47a.75.75 0 1 0 1.06 1.06L13.56 8 6.03.47a.75.75 0 0 0-1.06 0z"></path></svg>
-					</section>
-				</section>
-			</section>
-			
-		</header>
-	)
+  const navigate = useNavigate()
+  return (
+    <header className='app-header'>
+      <section className='header-container'>
+        <section className='pageing'>
+          <section className='page-backward' onClick={() => navigate(-1)}>
+            <svg
+              data-encore-id='icon'
+              role='img'
+              aria-hidden='true'
+              viewBox='0 0 16 16'
+            >
+              <path d='M11.03.47a.75.75 0 0 1 0 1.06L4.56 8l6.47 6.47a.75.75 0 1 1-1.06 1.06L2.44 8 9.97.47a.75.75 0 0 1 1.06 0z'></path>
+            </svg>
+          </section>
+          <section className='page-forward' onClick={() => navigate(1)}>
+            <svg
+              data-encore-id='icon'
+              role='img'
+              aria-hidden='true'
+              viewBox='0 0 16 16'
+            >
+              <path d='M4.97.47a.75.75 0 0 0 0 1.06L11.44 8l-6.47 6.47a.75.75 0 1 0 1.06 1.06L13.56 8 6.03.47a.75.75 0 0 0-1.06 0z'></path>
+            </svg>
+          </section>
+        </section>
+      </section>
+    </header>
+  )
 }
--- src/pages/StationDetails.jsx
+++ src/pages/StationDetails.jsx
@@ -1,6 +1,6 @@
 import { loadStation, updateSong } from '../store/actions/station.actions.js'
 import { useParams } from 'react-router-dom'
-import { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
@@ -8,8 +8,11 @@ import PlayIcon from '../assets/icons/play.svg?react'
 import AddIcon from '../assets/icons/addsong.svg?react'
 import SongOptionsIcon from '../assets/icons/song_options.svg?react'
 import LikedIcon from '../assets/icons/likedsong.svg?react'
+// import BackIcon from '../assets/icons/back.svg?react'
+// import ForwardIcon from '../assets/icons/forward.svg?react'
 
 import { SongOptionsModal } from '../cmps/SongOptionsModal.jsx'
+import { MoreModal } from '../cmps/MoreModal.jsx'
 
 export function StationDetails() {
   const { stationId } = useParams()
@@ -55,53 +58,65 @@ export function StationDetails() {
   if (!station) return <div>Loading...</div>
 
   return (
-    <div className='station-details-container'>
-      {/* <Link to='/' className='back-link'>
+    <React.Fragment>
+      <div className='station-details-container'>
+        {/* <AppHeader /> */}
+        {/* <Link to='/' className='back-link'>
         Back to list
       </Link> */}
-      <div className='station-header'>
-        <img
-          className='station-image'
-          src={station.createdBy.imgUrl}
-          alt={station.createdBy.fullname}
-        />
-        <div className='station-info'>
-          <h1 className='station-name'>{station.name}</h1>
-          <p className='station-creator'>
-            Created by {station.createdBy.fullname}
-          </p>
+        <div className='station-header'>
+          {/* <div className='nav-button' onClick={() => history.goBack()}>
+            <BackIcon className='nav-icon' />
+          </div>
+          <div className='nav-button' onClick={() => history.goForward()}>
+            <ForwardIcon className='nav-icon' />
+          </div> */}
+
+          <img
+            className='station-image'
+            src={station.createdBy.imgUrl}
+            alt={station.createdBy.fullname}
+          />
+          <div className='station-info'>
+            <h1 className='station-name'>{station.name}</h1>
+            <p className='station-creator'>
+              Created by {station.createdBy.fullname}
+            </p>
+          </div>
         </div>
+        <ul className='station-details'>
+          {station.songs.map((song) => (
+            <li key={song.id} className='song-item'>
+              <button className='play-button' onClick={() => onClickPlay(song)}>
+                <PlayIcon />
+              </button>
+              <img className='song-image' src={song.imgUrl} alt={song.title} />
+              <span className='song-info'>{song.title}</span>
+              <span></span> {/* Placeholder for song album */}
+              <span>{formatDate(song.addedAt)}</span>
+              <button className='add-button'>
+                <AddIcon />
+              </button>
+              <span className='song-length'>
+                {formatDuration(song.duration)}
+              </span>
+              <button
+                className='song-options-button'
+                onClick={(e) => handleOptionsClick(song, e.currentTarget)}
+              >
+                <SongOptionsIcon />
+              </button>
+            </li>
+          ))}
+        </ul>
+        {selectedSong && (
+          <MoreModal
+            song={selectedSong}
+            onClose={handleCloseModal}
+            buttonRef={buttonRef}
+          />
+        )}
       </div>
-      <ul className='station-details'>
-        {station.songs.map((song) => (
-          <li key={song.id} className='song-item'>
-            <button className='play-button' onClick={() => onClickPlay(song)}>
-              <PlayIcon />
-            </button>
-            <img className='song-image' src={song.imgUrl} alt={song.title} />
-            <span className='song-info'>{song.title}</span>
-            <span></span> {/* Placeholder for song album */}
-            <span>{formatDate(song.addedAt)}</span>
-            <button className='add-button'>
-              <AddIcon />
-            </button>
-            <span className='song-length'>{formatDuration(song.duration)}</span>
-            <button
-              className='song-options-button'
-              onClick={(e) => handleOptionsClick(song, e.currentTarget)}
-            >
-              <SongOptionsIcon />
-            </button>
-          </li>
-        ))}
-      </ul>
-      {selectedSong && (
-        <SongOptionsModal
-          song={selectedSong}
-          onClose={handleCloseModal}
-          buttonRef={buttonRef}
-        />
-      )}
-    </div>
+    </React.Fragment>
   )
 }
