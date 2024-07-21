b4e47c062361dbb1a725b4503d9a3d4427633493
Or Bracha <orbracha50@gmail.com>
2024-07-21 11:41:59 +0300

    Add create modal

--- src/cmps/SideLibrary.jsx
+++ src/cmps/SideLibrary.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { loadStations } from "../store/actions/station.actions"
 import { StationList } from "./StationList"
 import { useSelector } from "react-redux"
+import { CreateStationModal } from "./CreateStationModal"
 
 export function SideLibrary() {
     const stations = useSelector(storeState => storeState.stationModule.stations)
@@ -12,6 +13,17 @@ export function SideLibrary() {
                 throw err
             })
     }, [])
+    function onOpenModal(event){
+            const x = event.clientX
+            const y = event.clientY + 20
+            console.log(`Clicked at X=${x}, Y=${y}`)
+            const element = document.querySelector('.create-modal')
+            element.style.left = `${x}px`
+            element.style.top = `${y}px`
+            element.style.display = 'block'
+            modalIsOpen = true
+            console.log('hi')
+    }
     return (
         <section className="side-library">
             <section className="library-header">
@@ -19,9 +31,10 @@ export function SideLibrary() {
                     <svg className="library-svg" data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 24 24" ><path d="M3 22a1 1 0 0 1-1-1V3a1 1 0 0 1 2 0v18a1 1 0 0 1-1 1zM15.5 2.134A1 1 0 0 0 14 3v18a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V6.464a1 1 0 0 0-.5-.866l-6-3.464zM9 2a1 1 0 0 0-1 1v18a1 1 0 1 0 2 0V3a1 1 0 0 0-1-1z"></path></svg>
                     <h1>Your Library</h1>
                 </div>
-                <svg className="add-svg" data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16"><path d="M15.25 8a.75.75 0 0 1-.75.75H8.75v5.75a.75.75 0 0 1-1.5 0V8.75H1.5a.75.75 0 0 1 0-1.5h5.75V1.5a.75.75 0 0 1 1.5 0v5.75h5.75a.75.75 0 0 1 .75.75z"></path></svg>
+                <svg onClick={onOpenModal} className="add-svg" data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16"><path d="M15.25 8a.75.75 0 0 1-.75.75H8.75v5.75a.75.75 0 0 1-1.5 0V8.75H1.5a.75.75 0 0 1 0-1.5h5.75V1.5a.75.75 0 0 1 1.5 0v5.75h5.75a.75.75 0 0 1 .75.75z"></path></svg>
             </section>
             <StationList stations={stations}/>
+            <CreateStationModal/>
         </section>
     )
 }
--- src/cmps/SongPreview.jsx
+++ src/cmps/SongPreview.jsx
@@ -6,26 +6,40 @@ import { updateSong } from '../store/actions/station.actions'
 import { MoreModal } from './MoreModal'
 export function SongPreview({ song }) {
     const [songToAdd, setSongToAdd] = useState(null)
+    let modalIsOpen = false
     function onClickPlay(song, target) {
         console.log(target)
         let station = { title: song.title, id: song.videoId, imgUrl: song.thumbnail }
         updateSong(station)
     }
-    function onAddTo(event,) {
+    function onAddTo(event) {
         setSongToAdd(song)
-        const x = event.clientX 
-        const y = event.clientY +20
+        const x = event.clientX
+        const y = event.clientY + 20
         console.log(`Clicked at X=${x}, Y=${y}`)
         const element = document.querySelector('.more-modal')
         element.style.left = `${x}px`
         element.style.top = `${y}px`
         element.style.display = 'block'
+        modalIsOpen = true
+        console.log('hi')
     }
+   /*  document.addEventListener('click', function (event) {
+        var elModal = document.querySelector('.more-modal'); // Replace with your element's ID
+        var clickedInside = elModal.contains(event.target);
+        if (!clickedInside && modalIsOpen === true) {
+            // Clicked outside the element
+            console.log('hi')
+            modalIsOpen = false
+            elModal.style.display = 'none'
+            // Perform your desired action here
+        }
+    }) */
     return <article /* onClick={onActive} */ className="song-item">
         <img src={song.thumbnail} alt="" />
         <button onClick={({ target }) => onClickPlay(song, target)}><Play /></button>
         <h1>{song.title}</h1>
-            <AddIcon  onClick={onAddTo}/>
-            <MoreModal song={songToAdd}/>
+        <AddIcon onClick={onAddTo} />
+        <MoreModal song={songToAdd} />
     </article>
 }
