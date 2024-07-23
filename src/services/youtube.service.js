ec593a860ffdcee6bf08a37813c28bc69456bf60
Amit Gal <amitgal@hotmail.com>
2024-07-23 15:32:39 +0300

    more styling

--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -1,60 +1,59 @@
-import axios from "axios"
-import { loadFromStorage, saveToStorage } from "./util.service"
+import axios from 'axios'
+import { loadFromStorage, saveToStorage } from './util.service'
 
 const YT_API_KEY = import.meta.env.VITE_YT_API_KEY
 
 const YOUTUBE_CACHE = 'ytDB'
 
-const gVideosAmount = 1
+const gVideosAmount = 5
 
 let gYoutubeCache = loadFromStorage(YOUTUBE_CACHE) || {}
 
 export async function getVideos(searchVal) {
-	if (gYoutubeCache[searchVal]) {
-	  return Promise.resolve(gYoutubeCache[searchVal]);
-	}
-  
-	const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${gVideosAmount}&videoEmbeddable=true&type=video&key=${YT_API_KEY}&q=${searchVal}`;
-  
-	try {
-	  const response = await axios.get(url);
-	  let videos = response.data.items;
-  
-	  // Assuming getVideoDetails is an async function
-	  videos = await Promise.all(videos.map(video => getVideoDetails(video)));
-  
-	  gYoutubeCache[searchVal] = videos;
-	  saveToStorage(YOUTUBE_CACHE, gYoutubeCache);
-  
-	  return videos;
-	} catch (error) {
-	  console.error('Error fetching videos:', error);
-	  throw error; // re-throw the error after logging it
-	}
+  if (gYoutubeCache[searchVal]) {
+    return Promise.resolve(gYoutubeCache[searchVal])
   }
 
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${gVideosAmount}&videoEmbeddable=true&type=video&key=${YT_API_KEY}&q=${searchVal}`
+
+  try {
+    const response = await axios.get(url)
+    let videos = response.data.items
+
+    // Assuming getVideoDetails is an async function
+    videos = await Promise.all(videos.map((video) => getVideoDetails(video)))
+
+    gYoutubeCache[searchVal] = videos
+    saveToStorage(YOUTUBE_CACHE, gYoutubeCache)
+
+    return videos
+  } catch (error) {
+    console.error('Error fetching videos:', error)
+    throw error // re-throw the error after logging it
+  }
+}
+
 export function getVideoDetails(video) {
-	
-	const { id, snippet } = video
-	const { title, thumbnails } = snippet
-	const videoId = id.videoId
-	const thumbnail = thumbnails.default.url
-	return { videoId, title, thumbnail }
+  const { id, snippet } = video
+  const { title, thumbnails } = snippet
+  const videoId = id.videoId
+  const thumbnail = thumbnails.default.url
+  return { videoId, title, thumbnail }
 }
 
 export function getValidFormatWord(str) {
-	let strArr = str.split('')
-	let formatedArr = strArr.map(letter => {
-		return letter === ' ' ? '_' : letter
-	})
-	return formatedArr.join('')
+  let strArr = str.split('')
+  let formatedArr = strArr.map((letter) => {
+    return letter === ' ' ? '_' : letter
+  })
+  return formatedArr.join('')
 }
 
 export function clearHistory() {
-	gYoutubeCache = {}
-	saveToStorage(YOUTUBE_CACHE, gYoutubeCache)
+  gYoutubeCache = {}
+  saveToStorage(YOUTUBE_CACHE, gYoutubeCache)
 }
 
 export function getKeywords() {
-	return Object.keys(gYoutubeCache)
+  return Object.keys(gYoutubeCache)
 }
