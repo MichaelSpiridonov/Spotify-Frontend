d8400aaed7de24724f59d10d6b9cf5a49cdc0ff1
MichaelSpiridonov <michael.sp579@gmail.com>
2024-07-25 15:06:11 +0300

    Add Drag & Drop to Station List

--- src/cmps/SongList.jsx
+++ src/cmps/SongList.jsx
@@ -1,6 +1,15 @@
 import { SongPreview } from './SongPreview'
-
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 export function SongList({ songs, onAddTo, onClickPlay }) {
+  function onDragEnd(result) {
+    const { destination, source} = result
+    if (!destination) return
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return
+      const [removed] = songs.splice(source.index, 1);
+      songs.splice(destination.index, 0, [removed][0]);
+      setStations(songs)
+  }
+
   if(!songs) return
   return (
     <>
--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -2,20 +2,53 @@ import { useSelector } from 'react-redux'
 import { LikeSongsPreview } from './LikedSongsPreview'
 import { StationPreview } from './StationPreview'
 import { StationModal } from './modals/StationModal'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { loadStations } from '../store/actions/station.actions';
+import { saveToStorage } from '../services/util.service';
 
-export function StationList({ stations }) {
+export function StationList() {
+  const stations = useSelector((storeState) => storeState.stationModule.stations)
+  function onDragEnd(result) {
+    console.log(result)
+    const { destination, source} = result
+    if (!destination) return
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return
+      const [removed] = stations.splice(source.index, 1);
+      stations.splice(destination.index, 0, [removed][0]);
+      saveToStorage('stations', stations)
+      loadStations()
+  }
   const likedSongs = useSelector(storeState => storeState.stationModule.likedSongs)
   if(!stations) return
   return (
     <section className='station-list'>
       {likedSongs && <LikeSongsPreview />}
-      <section className='station-container'>
-        {stations.map((station) => (
-          <StationPreview key={station._id} station={station}/>
-        ))}
-      </section >
+      <DragDropContext onDragEnd={onDragEnd}>
+      {stations.map((station, index) => (
+      <Droppable droppableId={station._id} className='station-container'>
+      {(provided, snapshot) => (
+        <div
+        {...provided.droppableProps}
+        ref={provided.innerRef}
+      >
+          <Draggable key={station._id} draggableId={station._id} index={index}>
+            {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <StationPreview key={station._id} station={station}/>
+                    </div>
+                  )}
+                </Draggable>
+                {provided.placeholder}
+            </div>
+            )}
+        </Droppable>
+      ))}
+      </DragDropContext>
       <StationModal station={stations}/>
     </section>
-
   )
 }
