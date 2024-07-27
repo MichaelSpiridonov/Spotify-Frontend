3d290956ebd95129641db7b7ba7b4c0bca4cf130
Amit Gal <amitgal@hotmail.com>
2024-07-27 17:24:40 +0300

    Update StationModal to use isEditing instead of isEditModalOpen

--- src/cmps/modals/StationEditModal.jsx
+++ src/cmps/modals/StationEditModal.jsx
@@ -1,47 +1,80 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { updateStations } from '../../store/actions/station.actions'
 import ChooseImageIcon from '../../assets/icons/chooseimage.svg?react'
+import NewPlaylist from '../../assets/icons/newplaylist.svg?react'
+import { updateStations } from '../../store/actions/station.actions'
 
 export function StationEditModal({ station, onClose }) {
-  const [name, setName] = useState(station?.name || '')
-  const [description, setDescription] = useState(station?.description || '')
-  const dispatch = useDispatch()
+  const [name, setName] = useState(station.name) // State for station name
+  const [description, setDescription] = useState(station.description) // State for station description
+  const [isNameFocused, setIsNameFocused] = useState(false) // State for name input focus
+  const [isDescriptionFocused, setIsDescriptionFocused] = useState(false) // State for description input focus
 
   const handleSave = () => {
-    const updatedStation = { ...station, name, description }
-    dispatch(updateStations(updatedStation))
-    onClose()
+    const updatedStation = { ...station, name, description } // Update station with new name and description
+    updateStations(updatedStation) // Call action to update station
+    onClose() // Close modal after saving
   }
 
   return (
-    <div className='station-edit-modal'>
-      <div className='modal-content'>
-        <div className='modal-image'>
-          <img src={station.imgUrl} alt={station.name} />
-          <div className='choose-image'>
-            <ChooseImageIcon />
+    <div className='edit-modal-backdrop'>
+      <dialog open className='station-edit-modal'>
+        <button className='close-button' onClick={onClose}>
+          ⨉
+        </button>{' '}
+        {/* Close button for modal */}
+        <h2>Edit details</h2>
+        <div className='modal-content'>
+          <div className='image-container'>
+            <img
+              src={station.imgUrl || NewPlaylist} // Display station image or default image
+              alt='Station Cover'
+              className='station-image'
+            />
+            <div className='choose-image'>
+              <ChooseImageIcon /> {/* Icon to choose a new image */}
+            </div>
+          </div>
+          <div className='details-container'>
+            <div className={`input-wrapper ${isNameFocused ? 'focused' : ''}`}>
+              <input
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)} // Update name state on change
+                onFocus={() => setIsNameFocused(true)}
+                onBlur={() => setIsNameFocused(false)}
+                placeholder='My Playlist #7'
+              />
+              <label>Name</label>
+            </div>
+            <div
+              className={`input-wrapper ${
+                isDescriptionFocused ? 'focused' : ''
+              }`}
+            >
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)} // Update description state on change
+                onFocus={() => setIsDescriptionFocused(true)}
+                onBlur={() => setIsDescriptionFocused(false)}
+                placeholder='Add an optional description'
+              />
+              <label>Description</label>
+            </div>
           </div>
         </div>
-        <div className='modal-info'>
-          <input
-            type='text'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder='Station Name'
-            className='station-name'
-          />
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder='Description'
-            className='station-description'
-          />
-          <button onClick={handleSave} className='save-button'>
+        <div className='modal-footer'>
+          <button className='save-button' onClick={handleSave}>
             Save
-          </button>
+          </button>{' '}
+          {/* Save button */}
+          <p>
+            By proceeding, you agree to give Spotify access to the image you
+            choose to upload. Please make sure you have the right to upload the
+            image.
+          </p>{' '}
+          {/* Disclaimer text */}
         </div>
-      </div>
+      </dialog>
     </div>
   )
 }
