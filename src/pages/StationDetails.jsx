6f732a74645f39bb76cbbb7e8977630a51a9b893
Or Bracha <orbracha50@gmail.com>
2024-08-01 16:28:01 +0300

    fix phone header

--- src/pages/StationDetails.jsx
+++ src/pages/StationDetails.jsx
@@ -137,11 +137,15 @@ export function StationDetails() {
   const gradientStyle = {
     backgroundImage: `linear-gradient(${color}, #121212 90%)`
   }
+  if(pageWidth<500){
+    if( document.querySelector('.user-login')) document.querySelector('.user-login').style.display='none'
+   
+  }
   return (
     <React.Fragment>
       <div  className='station-details-container'>
         <section style={gradientStyle}>
-          <AppHeader color={color} />
+         { pageWidth> 500 && <AppHeader  color={color} />}
           <div className='station-header'>
             {currStation.imgUrl && <img
               className='station-image'
